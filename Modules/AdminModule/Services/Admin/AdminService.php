79845fef7ef1753391d7ea9347740cd6ca44e0bc
AppRoc <emanmoharam35@gmail.com>
2022-10-09 16:40:06 +0200

    recode

--- Modules/AdminModule/Services/Admin/AdminService.php
+++ Modules/AdminModule/Services/Admin/AdminService.php
@@ -1,1 +1,21 @@
 <?php
+namespace Modules\AdminModule\Services\Admin;
+
+use Modules\AdminModule\Repositories\Admin\AdminRepository;
+
+class AdminService
+{
+    use AdminServiceHelper,UploadHelper;
+
+    protected $adminRepository;
+
+    public function __construct()
+    {
+        $adminRepository = new AdminRepository();
+    }
+
+    public function login(array $data){
+
+    }
+
+}
