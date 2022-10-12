1c8c11c410599ebb50631dc7018938450cb44e2b
unknown <mahmodsaadlebda@gmail.com>
2022-10-12 10:04:11 +0200

    recode

--- Modules/AdminModule/Services/Admin/AdminServiceHelper.php
+++ Modules/AdminModule/Services/Admin/AdminServiceHelper.php
@@ -2,6 +2,7 @@
 
 namespace Modules\AdminModule\Services\Admin;
 
+use Illuminate\Validation\Rule;
 use Modules\AdminModule\Entities\Admin;
 use Modules\CommonModule\Entities\ResetPassword;
 
@@ -27,7 +28,7 @@ trait AdminServiceHelper{
             'password' =>'required|min:12|confirmed',
             'role_id' => 'required|exists:roles,id',
             'email' => 'required|email|unique:admins,email,id',
-            'phone' => 'required|numeric|digits:11,unique:admins,phone',
+            'phone' => 'required|numeric|digits:11|unique:admins,phone',
             'image' => 'nullable|image',
             'type' => 'exists:roles,title'
         ]);
@@ -36,11 +37,11 @@ trait AdminServiceHelper{
         return validator($data,[
             'id'=>'required|exists:admins,id',
             'name'=>'required',
-            'username' => 'required|alpha_num|unique:admins,username',
+            'username' => 'required|alpha_num|alpha_num|unique:admins,username,'.$data['id'],
             'password' =>'nullable|min:12|confirmed',
             'role_id' => 'required|exists:roles,id',
-            'email' => 'required|email|unique:admins,email,id',
-            'phone' => 'required|numeric|digits:11,unique:admins,phone',
+            'email' => ['required', 'email', Rule::unique('admins')->ignore($data['id'])],
+            'phone' => ['nullable','numeric','digits:12', Rule::unique('admins','phone')->ignore($data['id'])],
             'image' => 'nullable|image',
             'type' => 'exists:roles,title'
         ]);
@@ -56,54 +57,5 @@ trait AdminServiceHelper{
             'password' => 'required|min:12|confirmed',
         ]);
     }
-    public function forgetPassword(array $data)
-    {
-        $validation = $this->validationForgetPassword($data);
-        if ($validation->fails()){
-            return return_msg(false,'Validation Errors',[
-                'validation_errors' => $validation->getMessageBag()->getMessages(),
-            ]);
-        }
-        $token = strtoupper(uniqid());
-        ResetPassword::where('identifier',$data['email'])->delete();
 
-        ResetPassword::create(['identifier' => $data['email'] , 'token' => $token]);
-
-        $url = route('getResetPasswordSite',['token' => $token]);
-
-        send_email([
-            'to' => $data['email'],
-            'content' => ['url' => $url],
-            'subject' => "Reset Password",
-            'template' => 'mails.reset_password',
-        ]);
-        return  return_msg(true,'Success');
-
-
-    }
-    public function resetPassword(array $data)
-    {
-        $validation = $this->validationResetPassword($data);
-        if ($validation->fails()){
-            return return_msg(false,'Validation Errors',[
-                'validation_errors' => $validation->getMessageBag()->getMessages(),
-            ]);
-        }
-
-        $reset = ResetPassword::where('token',$data['token'])->first();
-
-        if (!$reset){
-            return return_msg(false,'Validation Errors',[
-                'validation_errors' => [
-                    'password'=> ["No User Found"]
-                ],
-            ]);
-        }
-        $admin = Admin::where('email',$reset->identifier)->first();
-        $admin->update(['password' => bcrypt($data['password'])]);
-        ResetPassword::where('token',$data['token'])->delete();
-
-        return  return_msg(true,'Success');
-
-    }
 }
